f08e05557b50382bbddbedb285f932c202c3e46e
Timur Khaimovich <t_khaimovich@outlook.com>
2021-05-24 17:50:54 +0600

    zabyl dlya heroku

--- botnet/settings.py
+++ botnet/settings.py
@@ -96,4 +96,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
