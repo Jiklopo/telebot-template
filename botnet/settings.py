3dc3439d159a3acd4f1144735c6a7f1e0182def0
Timur Khaimovich <t_khaimovich@outlook.com>
2021-06-06 17:03:18 +0600

    refactored settings.py

--- botnet/settings.py
+++ botnet/settings.py
@@ -1,7 +1,6 @@
+import django_heroku
 from os import getenv
 from pathlib import Path
-
-import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY') or getenv('TOKEN') or 'very_secret_key'
@@ -100,20 +99,11 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'controls'
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 django_heroku.settings(locals(), logging=False)
